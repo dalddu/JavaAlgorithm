f0d05f4949eedf10dcf54dc05e5bca4a7e81556e
DESKTOP-J9CMK4R\USER <0313tndus@naver.com>
2022-11-08 23:11:03 +0900

    BinarySearch(이진탐색)01

---
+++ src/main/java/_1108/BinarySearch01.java
@@ -0,0 +1,11 @@
+package _1108;
+
+public class BinarySearch01 {
+    public static void main(String[] args) {
+        int[] numbers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        int midIndex = numbers.length / 2;
+        System.out.println(numbers.length);
+        System.out.println(midIndex);
+
+    }
+}
